fa217b0e8eabe001fa2c576fa5416a07782ce74b
Giovanna <giovannamsantanaa@gmail.com>
2022-11-08 15:06:27 -0300

    Criando os principais componentes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,36 @@
 function HomePage() {
     const estiloDaHomePage = { backgroundColor: "red" };
     return (
-        <div style={estiloDaHomePage}>Bem vindo ao AluraTube!</div>
+        <div style={estiloDaHomePage}>
+            <Menu></Menu>
+            <Header></Header>
+            <Timeline></Timeline>
+        </div>
     );
 }
 
 export default HomePage
+
+function Menu() {
+    return (
+        <div>
+            Menu
+        </div>
+    )
+}
+
+function Header() {
+    return (
+        <div>
+            Header
+        </div>
+    )
+}
+
+function Timeline() {
+    return (
+        <div>
+            Timeline
+        </div>
+    )
+}
